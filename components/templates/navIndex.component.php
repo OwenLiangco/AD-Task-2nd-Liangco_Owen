c67d72ec4303c96f6afcfda5571081fdeeb25ced
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 09:29:53 +0800

    feat(templates): added a ul class named nav links for the nav bar

--- components/templates/navIndex.component.php
+++ components/templates/navIndex.component.php
@@ -1,6 +1,10 @@
 <header>
     <nav class = "navbar">
         <div class = "logo">Bluu Cloud Reviews</div>
-        
+    <div>
+        <ul class = "nav-links" id = "navLinks">
+            
+        </ul>
+    </div>
     </nav>
 </header>
