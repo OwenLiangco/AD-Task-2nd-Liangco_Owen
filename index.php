a87b90817f6316d6575504ded13a9bf6f585a2cd
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 09:55:46 +0800

    feat(index): added a brief welcome for the index page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once BASE_PATH . '/components/templates/footIndex.component.php';
 
 <secion class = "hero">
     <div clas = "hero-content">
-        
+        <h1>Welcome to My Website!</h1>
+        <p>Click here to see my archived review posts.</p>
     </div>
 </secion>
