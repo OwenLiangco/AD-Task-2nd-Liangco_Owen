5e72aa23977301cbc59f9a806534266863d9ac2c
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 09:20:05 +0800

    feat(templates): added the upper index html, and added the href path to the css

---
+++ components/templates/headIndex.component.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/assets/css/example.css"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bluu Cloud Reviews</title>
+</head>
+<body>
