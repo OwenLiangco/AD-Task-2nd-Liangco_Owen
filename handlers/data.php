fae3f537c37e5a7c279a1f006d8b3e2946e1c559
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 19:04:02 +0800

    Fix(handlers): corrected the img paths for the game images

--- handlers/data.php
+++ handlers/data.php
@@ -4,35 +4,35 @@ $games = [
         "title" => "Mother 3",
         "year" => 2005,
         "console" => "GBA",
-        "image" => "../page/ReviewPage/assets/img/motherThree.jpg",
+        "image" => "/page/ReviewPage/assets/img/motherThree.jpg",
         "review" => "An emotional and unforgettable RPG experience with deep story-telling. I played this game during the pandemic and I fell in love with the world-building and the rich story-telling, I highly recommend this if you want an amazing RPG experience."
     ],
     [
         "title" => "The Legend of Zelda: The Minish Cap",
         "year" => 2004,
         "console" => "GBA",
-        "image" => "../page/ReviewPage/assets/img/LOZminishCap.jpg",
+        "image" => "/page/ReviewPage/assets/img/LOZminishCap.jpg",
         "review" => "A charming and creative entry in the Zelda series with great mechanics. The first Zelda game I played and what hooked me in the series! I love the puzzles of the dungeons and each gimmick."
     ],
     [
         "title" => "Advance Wars",
         "year" => 2001,
         "console" => "GBA",
-        "image" => "../page/ReviewPage/assets/img/AdvanceWars.jpg",
+        "image" => "/page/ReviewPage/assets/img/AdvanceWars.jpg",
         "review" => "A brilliant strategy game with addictive, tactical gameplay. This is the first game that introduced Me to tactical games, haven't played it in a while; but I still hold it with high regards cause of the strategy."
     ],
     [
         "title" => "Kirby & The Amazing Mirror",
         "year" => 2004,
         "console" => "GBA",
-        "image" => "../page/ReviewPage/assets/img/KatAM.jpg",
+        "image" => "/page/ReviewPage/assets/img/KatAM.jpg",
         "review" => "A unique Metroidvania-style Kirby game packed with exploration. I LOVE THIS GAME TO MY VERY SOUL- This was my very first exposure to the Kirby series! The way you can branch out into different zones and fight bosses whichever order you want to start first. Its exploration gimmick is where the game truly shines for Me, it may be hard at first but as soon as you know each way, it will feel easy to maneuver."
     ],
     [
         "title" => "Pokémon Emerald",
         "year" => 2004,
         "console" => "GBA",
-        "image" => "../page/ReviewPage/assets/img/PokemonEmeraldBox.jpg",
+        "image" => "/page/ReviewPage/assets/img/PokemonEmeraldBox.jpg",
         "review" => "A fan-favorite game with epic post-game content. This was my very first Pokémon game, the region, the soundtrack, the pokémon, and everything! Pokémon Emerald will be my forever go-to Pokémon game and would highly recommend to others."
     ]
 ];
