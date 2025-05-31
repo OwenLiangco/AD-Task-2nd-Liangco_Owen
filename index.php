378c36ae48c859fcc03e02cb6b42f8b1c1ced171
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 09:52:45 +0800

    feat(index): added a section class for the hero section

--- index.php
+++ index.php
@@ -5,3 +5,7 @@ require_once BASE_PATH . '/components/templates/headIndex.component.php';
 require_once BASE_PATH . '/components/templates/navIndex.component.php';
 require_once BASE_PATH . '/components/templates/footIndex.component.php';
 ?>
+
+<secion class = "hero">
+    
+</secion>
