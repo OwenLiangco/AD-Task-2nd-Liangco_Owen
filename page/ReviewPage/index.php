df3f7abe412e4491900cc8e3f757ee3da158abf4
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 14:35:11 +0800

    feat(index): added the php for the base path and autoload for the review page

---
+++ page/ReviewPage/index.php
@@ -0,0 +1,7 @@
+<?php
+require BASE_PATH . '/vendor/autoload.php';
+
+require_once BASE_PATH . '/components/templates/reviewPageTemplates/headReview.component.php';
+require_once BASE_PATH . '/components/templates/reviewPageTemplates/navReview.component.php';
+require_once BASE_PATH . '/components/templates/reviewPageTemplates/footReview.component.php';
+?>
