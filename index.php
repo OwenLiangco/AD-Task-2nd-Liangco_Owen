d2b17699d2d90bb850aeb2fb962e1f4094454ec8
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 09:51:48 +0800

    feat(index): added the php for the base path of the fragmented components and autoload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+require BASE_PATH . '/vendor/autoload.php';
+
+require_once BASE_PATH . '/components/templates/headIndex.component.php';
+require_once BASE_PATH . '/components/templates/navIndex.component.php';
+require_once BASE_PATH . '/components/templates/footIndex.component.php';
+?>
