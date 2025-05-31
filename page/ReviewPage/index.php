e091697e590fd8a3e5e825b7065742a878bab6fd
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 18:14:07 +0800

    feat(index): added the php base path for the data from handlers, and the game card component from the components

--- page/ReviewPage/index.php
+++ page/ReviewPage/index.php
@@ -4,4 +4,7 @@ require BASE_PATH . '/vendor/autoload.php';
 require_once BASE_PATH . '/components/templates/reviewPageTemplates/headReview.component.php';
 require_once BASE_PATH . '/components/templates/reviewPageTemplates/navReview.component.php';
 require_once BASE_PATH . '/components/templates/reviewPageTemplates/footReview.component.php';
+
+require_once BASE_PATH . '/handlers/data.php';
+require_once BASE_PATH . '/components/templates/cards/gameCard.component.php';
 ?>
