b3823e340dfcdbc3a15e92d1086881f2d0cce2d1
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 09:22:03 +0800

    fix(templates): fixed the positions of the meta tags and position of the title tags

--- components/templates/headIndex.component.php
+++ components/templates/headIndex.component.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="/assets/css/example.css"
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bluu Cloud Reviews</title>
+    <link rel="stylesheet" href="/assets/css/example.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
