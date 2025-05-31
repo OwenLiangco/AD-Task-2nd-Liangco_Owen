1949b973d2d1b8fce6a4afed05014e918f327d91
Owen Liangco <owenliangco21@gmail.com>
2025-05-31 14:21:15 +0800

    feat(templates): added the nav component for the review page, added nav bar and burger code from before

---
+++ components/templates/reviewPageTemplates/navReview.component.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class = "navbar">
+        <div class = "logo">Bluu Cloud Reviews</div>
+    <div>
+        <ul class = "nav-links" id = "navLinks">
+            <li><a href = "/index.php">Home</a></li>
+            <li><a href = "/page/ReviewPage/index.php">Reviews</a></li>
+            <li><a href = "#about">About</a></li>
+            <li><a href = "#contact">Contact</a></li>
+        </ul>
+    </div>
+    <div>
+        <div class = "burger" id = "burger">
+            <div class = "line1"></div>
+            <div class = "line2"></div>
+            <div class = "line3"></div>
+        </div>
+    </div>
+    </nav>
+</header>
